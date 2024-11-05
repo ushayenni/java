790373789ba9ae4819973b7d0869af240c3f9d74
ushayenni <151708074+ushayenni@users.noreply.github.com>
2024-11-05 15:32:31 +0530

    Create StringBuffer.java

---
+++ StringBuffer.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner.*;
+class StringBufferDemo{
+    public static void main(String[] args) {
+        StringBuffer sb1=new StringBuffer("Hello");
+        sb1.append("Java");
+        System.out.println("The string is"+sb1);
+        StringBuffer sb2=new StringBuffer("Hello");
+        sb2.insert(2,"java");
+        System.out.println("The string is"+sb2);
+        StringBuffer sb3=new StringBuffer("Hello");
+        sb3.replace(1,3,"java");
+        System.out.println("The String is"+sb3);
+        StringBuffer sb4=new StringBuffer("hello");
+        sb4.delete(1,3);
+        System.out.println("The string is "+sb4);
+        StringBuffer sb5=new StringBuffer("Hello");
+        sb5.reverse();
+        System.out.println("The string is"+sb5);
+    }
+}
